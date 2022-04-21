b6775cab09a2f578993497826644951c603ac93f
Sanskriti <55571542+sans1012@users.noreply.github.com>
2022-04-21 20:24:04 +0530

    Add files via upload

--- input.py
+++ input.py
@@ -3,7 +3,9 @@ from scipy.io.wavfile import write
 
 fs=16000
 print("Say YES")
-record_voice= sd.rec( int ( 3 * fs ) , samplerate = fs , channels = 2 )
+record_voice= sd.rec( int ( 1 * fs ) , samplerate = fs , channels = 2 )
+
 sd.wait()
-write("out.wav",fs,record_voice)
+
+write("out6.wav",fs,record_voice)
 print ("Done.")
